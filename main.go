1d84ef4f1278bda8f756549294ea47e1c81e72e5
李文龙 <285753421@qq.com>
2019-10-11 18:05:23 +0800

    etcd

--- main.go
+++ main.go
@@ -8,8 +8,11 @@ import (
     "runtime"
     _ "net/http/pprof"
     c "ws/component"
+    "github.com/go-libraries/timer"
+    "time"
 )
 var addr = flag.String("addr", "0.0.0.0:8080", "http service address")
+var etcdServer = flag.String("etcdServer", "0.0.0.0:2380", "etcd service address")
 
 func main()  {
     runtime.GOMAXPROCS(runtime.NumCPU())
@@ -17,5 +20,11 @@ func main()  {
     log.SetFlags(0)
     ws.Wsp.PLog = c.Logs
     go c.GetMessage(ws.Wsp)
+
+    etcd
+    sc := timer.GetTaskScheduler()
+    sc.AddFuncSpace(int64(20*time.Second), time.Now().Unix() + int64(time.Hour*10000), func() {
+
+    })
     log.Fatal(http.ListenAndServe(*addr, ws.Wsp))
 }
